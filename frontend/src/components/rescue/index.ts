9f5b4e801760ed87bc75f7f77618ab47b0d8e07b
internship68 <sunthinnakorn002@gmail.com>
2025-12-02 21:57:29 +0700

    Remove frontend components, configurations, and assets in preparation for a complete overhaul of the emergency management system. Added VSCode settings to ignore Git limit warnings.

--- frontend/src/components/rescue/index.ts
+++
@@ -1,7 +0,0 @@
-export { StatCard } from './StatCard';
-export { CaseCard } from './CaseCard';
-export { TeamCard } from './TeamCard';
-export { ConnectionStatus } from './ConnectionStatus';
-export { EmptyState } from './EmptyState';
-export { DashboardSkeleton, StatCardSkeleton, CaseCardSkeleton, TeamCardSkeleton } from './LoadingSkeleton';
-
